0cf4ec1c948cf70bf454b029f71ab3b50b26bcba
AarkayITsolutions <alihassanali119683@gmail.com>
2025-02-25 09:14:13 +0500

    adding files

---
+++ src/main/java/com/example/order_api/OrderManagementApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.order_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManagementApiApplication.class, args);
+    }
+}
